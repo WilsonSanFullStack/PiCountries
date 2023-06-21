c0e3ce7723c7e580a4c46f39bb8bb5efd902788c
ryuksan <clinicasystemlab@gmail.com>
2023-06-20 20:55:34 -0500

    actualizacion

--- server/src/db.js
+++ server/src/db.js
@@ -3,28 +3,24 @@ const { Sequelize } = require("sequelize");
 
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: false,
   native: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-    },
-  },
 });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-fs.readdirSync(path.join(__dirname, "../../db/models"))
+fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
     (file) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "../../db/models", file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 modelDefiners.forEach((model) => model(sequelize));
